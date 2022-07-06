3d6df4f023ad65ca1be6ef3187e07cbfde8fb052
RohitSquareops <107384244+RohitSquareops@users.noreply.github.com>
2022-07-06 11:41:17 +0530

    Update agent.js

--- src/agent.js
+++ src/agent.js
@@ -3,7 +3,7 @@ import _superagent from 'superagent';
 
 const superagent = superagentPromise(_superagent, global.Promise);
 
-const API_ROOT = process.env.DOMAIN_URL;
+const API_ROOT = 'https://rohitsquare.rtd.squareops.co.in/api';
 
 const encode = encodeURIComponent;
 const responseBody = res => res.body;
